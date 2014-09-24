58a2b6ea0e4f7c2ea04293373ea2ff0713c8e2ff
YoshiteruIwasaki <yoshiteru.i@gmail.com>
2014-09-24 23:11:48 +0900

    table view

--- app/controllers/Api.java
+++ app/controllers/Api.java
@@ -11,6 +11,31 @@ import play.mvc.Result;
 
 public class Api extends Controller {
 
+	public static Promise<Result> index(String type) {
+		String apikey = Play.application().configuration()
+				.getString("application.apikey");
+		StringBuilder builder = new StringBuilder();
+		builder.append("https://api.tokyometroapp.jp/api/v2/datapoints?");
+		builder.append("rdf:type=");
+		builder.append(type);
+		builder.append("&");
+		builder.append("acl:consumerKey=");
+		builder.append(apikey);
+
+		Logger.debug(builder.toString());
+
+		final Promise<Result> resultPromise = WS.url(builder.toString()).get()
+				.map(new Function<WSResponse, Result>() {
+					public Result apply(WSResponse response) {
+						if (response.getStatus() == 200) {
+							return ok(response.asJson());
+						}
+						return ok("");
+					}
+				});
+		return resultPromise;
+	}
+
 	public static Promise<Result> trainInformation() {
 		String apikey = Play.application().configuration()
 				.getString("application.apikey");
